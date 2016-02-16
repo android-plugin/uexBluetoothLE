8bb42cfb03941db6a8fdf62d5aa0676750773921
yanlongtao <longtao.yan@zymobi.com>
2016-02-16 09:50:24 +0800

    修改回调函数名称与文档一致

--- src/org/zywx/wbpalmstar/plugin/uexbluetoothle/JsConst.java
+++ src/org/zywx/wbpalmstar/plugin/uexbluetoothle/JsConst.java
@@ -2,9 +2,9 @@ package org.zywx.wbpalmstar.plugin.uexbluetoothle;
 
 public class JsConst {
     public static final String ON_CONNECTION_STATE_CHANGE = "uexBluetoothLE.onConnectionStateChange";
-    public static final String ON_CHARACTERISTIC_READ = "uexBluetoothLE.cbCharacteristicRead";
+    public static final String ON_CHARACTERISTIC_READ = "uexBluetoothLE.cbReadCharacteristic";
     public static final String ON_CHARACTERISTIC_CHANGED = "uexBluetoothLE.onCharacteristicChanged";
-    public static final String ON_CHARACTERISTIC_WRITE = "uexBluetoothLE.cbCharacteristicWrite";
+    public static final String ON_CHARACTERISTIC_WRITE = "uexBluetoothLE.cbWriteCharacteristic";
     public static final String ON_LE_SCAN = "uexBluetoothLE.onLeScan";
     public static final String CALLBACK_CONNECT = "uexBluetoothLE.cbConnect";
     public static final String CALLBACK_SEARCH_FOR_CHARACTERISTIC = "uexBluetoothLE.cbSearchForCharacteristic";
